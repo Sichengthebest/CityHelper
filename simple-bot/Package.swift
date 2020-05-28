80f224af6ad3b222ffa4d689275c05206b740117
Danny Huang <huangdong@gmail.com>
2020-05-28 14:36:01 -0400

    一个可以正常build和运行的项目

--- simple-bot/Package.swift
+++ simple-bot/Package.swift
@@ -5,18 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "simple-bot",
+    products: [
+        .executable(
+            name: "simple-bot",
+            targets: ["simple-bot"]
+        ),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "TelegramBotSDK", url: "https://github.com/zmeyc/telegram-bot-swift.git", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "simple-bot",
-            dependencies: []),
-        .testTarget(
-            name: "simple-botTests",
-            dependencies: ["simple-bot"]),
+            dependencies: ["TelegramBotSDK"]),
     ]
 )
