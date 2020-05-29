3e5184fc233c899ef12889cb6858bb9dd1670721
Danny Huang <huangdong@gmail.com>
2020-05-29 11:42:40 -0400

    给simple-bot增加一个发送用户信息的功能

--- simple-bot/Sources/simple-bot/main.swift
+++ simple-bot/Sources/simple-bot/main.swift
@@ -8,6 +8,8 @@ while let update = bot.nextUpdateSync() {
     if let message = update.message, let from = message.from, let text = message.text {
         bot.sendMessageAsync(chatId: ChatId.chat(from.id),
                              text: "Hi \(from.firstName)! You said: \(text).\n")
+        bot.sendMessageAsync(chatId: ChatId.chat(from.id), text:
+            "ID:\(from.id)\nusername:\(from.username ?? "")\nlanguageCode:\(from.languageCode ?? "")\nfirstName:\(from.firstName)")
     }
 }
 
