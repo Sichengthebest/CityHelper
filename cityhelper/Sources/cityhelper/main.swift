890053d6729da7e7f40eb4000ec212223b13860c
Danny Huang <huandong@gmail.com>
2020-06-02 21:08:05 -0400

    进一步简化代码

--- cityhelper/Sources/cityhelper/main.swift
+++ cityhelper/Sources/cityhelper/main.swift
@@ -20,7 +20,7 @@ func onCallbackQueryc(context: Context) -> Bool {
             replyMarkup: ReplyMarkup.inlineKeyboardMarkup(markup)
         )
     }else{
-        context.respondAsync("垃圾在周四（法定节假日除外）从早上7:00到下午6:00收集。应在收集日当天的上午7:00前或前一天的晚上9:00后，将垃圾桶放到路边。确保在晚上11:00之前从路边收回垃圾箱", replyMarkup: ReplyMarkup.inlineKeyboardMarkup(markup))
+        context.respondAsync("垃圾在周四（法定节假日除外）从早上7:00到下午6:00收集。应在收集日当天的上午7:00前或前一天的晚上9:00后，将垃圾桶放到路边。确保在晚上11:00之前从路边收回垃圾箱", replyMarkup: .inlineKeyboardMarkup(markup))
     }
     return true
 }
@@ -32,7 +32,7 @@ func onCallbackQuerye(context: Context) -> Bool {
         chatId: .chat(context.chatId!),
         messageId: context.message?.messageId,
         text: "Garbage is picked up Thursdays (except statutory holidays) from 7:00 a.m. to 6:00 p.m. The bin should be put out by the curb after 9:00 p.m. the night before or before 7:00 a.m. the day of the collection. Make sure to take the bin back from the curb the same day before 11:00 p.m.",
-        replyMarkup: ReplyMarkup.inlineKeyboardMarkup(markup)
+        replyMarkup: .inlineKeyboardMarkup(markup)
     )
     return true
 }
@@ -44,7 +44,7 @@ func onCallbackQueryf(context: Context) -> Bool {
         chatId: .chat(context.chatId!),
         messageId: context.message?.messageId,
         text: "Les jours de collecte sont les jeudis de 7 h à 18 h, sauf les jours fériés. Les bacs doivent être placés la veille à partir de 21 h ou avant 7 h le jour de la collecte, et doivent être rentrés le même jour avant 23 h.",
-        replyMarkup: ReplyMarkup.inlineKeyboardMarkup(markup)
+        replyMarkup: .inlineKeyboardMarkup(markup)
     )
     return true
 }
