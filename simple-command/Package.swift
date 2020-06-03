a07ee39a0952c07359c4e0c10b15c03d69d1e321
hdcola <huandong@gmail.com>
2020-06-03 09:04:56 -0400

    所有的Swift Package都升级到官方2.1.0版本

--- simple-command/Package.swift
+++ simple-command/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "TelegramBotSDK", url: "https://github.com/HDCodePractice/telegram-bot-swift.git", from: "2.0.0"),
+        .package(name: "TelegramBotSDK", url: "https://github.com/rapierorg/telegram-bot-swift.git", from: "2.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
